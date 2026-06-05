8f48124faa6f2c39a38f4086b81550e4bbbc35e1
Gergov00 <gergovzaurbek@gmail.com>
2026-06-06 00:02:30 +0300

    add di in main

--- backend/auth-service/cmd/auth/main.go
+++ backend/auth-service/cmd/auth/main.go
@@ -1,9 +1,91 @@
 package main
 
 import (
-	"fmt"
+	"auth-service/internal/config"
+	"auth-service/internal/domain"
+	"auth-service/internal/repository"
+	"auth-service/internal/service"
+	"auth-service/internal/token"
+	"auth-service/internal/transport/web"
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"go.uber.org/fx"
 )
 
 func main() {
-	fmt.Println("Hello, Auth Service!")
+	godotenv.Load()
+	app := fx.New(
+		fx.Provide(
+			config.Load,
+			fx.Annotate(repository.NewCredential, fx.As(new(domain.CredentialRepository))),
+			fx.Annotate(repository.NewRefresh, fx.As(new(domain.RefreshTokenRepository))),
+			fx.Annotate(repository.NewTxManager, fx.As(new(domain.TxManager))),
+			fx.Annotate(NewTokerProvider, fx.As(new(domain.TokenProvider))),
+			web.NewRouter,
+			NewAuthHandlerProvider,
+			NewDataBase,
+			service.NewAuthService,
+		),
+		fx.Invoke(Run),
+	)
+	app.Run()
+}
+
+func Run(lc fx.Lifecycle, router http.Handler, cfg *config.Config) {
+	srv := &http.Server{
+		Addr:    ":" + cfg.HTTPPort,
+		Handler: router,
+	}
+
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			log.Println("Starting server...")
+			go func() {
+				if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+					log.Fatalf("listen: %s\n", err)
+				}
+				
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			log.Println("Stopping server...")
+			return srv.Shutdown(ctx)
+		},
+	})
+
+}
+
+func NewDataBase(lc fx.Lifecycle, cfg *config.Config) (*pgxpool.Pool, error) {
+	pool, err := pgxpool.New(context.Background(), cfg.DBURL)
+	if err != nil {
+		log.Fatalf("db pool: %v", err)
+		return nil, err
+	}
+
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			log.Println("Data base to conection...")
+			return pool.Ping(ctx)
+		},
+		OnStop: func(ctx context.Context) error {
+			log.Println("Closing data base connection...")
+			pool.Close()
+			return nil
+		},
+	})
+
+	return pool, nil
+}
+
+func NewAuthHandlerProvider(cfg *config.Config, service *service.AuthService) *web.AuthHandler {
+	return web.NewAuthHandler(service, cfg.RefreshTTL, cfg.CookieSecure)
+}
+
+func NewTokerProvider(cfg *config.Config) *token.Manager {
+	return token.NewManager(cfg.JWTSecret, cfg.AccessTTL, cfg.RefreshTTL, cfg.Issuer)
 }
--- backend/auth-service/go.mod
+++ backend/auth-service/go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgerrcode v0.0.0-20250907135507-afb5586c32a6
 	github.com/jackc/pgx/v5 v5.10.0
+	github.com/joho/godotenv v1.5.1
+	go.uber.org/fx v1.24.0
 	golang.org/x/crypto v0.52.0
 )
 
@@ -14,6 +16,10 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	go.uber.org/dig v1.19.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/sync v0.20.0 // indirect
+	golang.org/x/sys v0.45.0 // indirect
 	golang.org/x/text v0.37.0 // indirect
 )
